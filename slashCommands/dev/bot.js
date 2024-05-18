dbcf607371f625d20f9f2fb6aad3448b29b579a9
Nox <patrik.tuominen@outlook.com>
2024-05-18 04:15:19 +0300

    ✨ feat: New command /bot

---
+++ slashCommands/dev/bot.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+const {glob} = require('glob')
+const { ApplicationCommandType, EmbedBuilder, AttachmentBuilder } = require("discord.js");
+const moment = require('moment');
+const con = require('../../function/db')
+
+module.exports = {
+    name: 'bot',
+    description: 'Display information about the bot.',
+    cooldown: 3000,
+    type: ApplicationCommandType.ChatInput,
+
+    run: async (client, interaction) => {
+        const { member, channelId, guildId, applicationId, 
+            commandName, deferred, replied, ephemeral, 
+            options, id, createdTimestamp 
+        } = interaction; 
+        const { guild } = member;
+
+        const now = moment();
+        const started = moment("21042024", "DDMMYYYY")
+        const daysCoded = started.fromNow();
+        const avghour = 3;
+        const daysBetween = moment.duration(now.diff(started)).asDays();
+        console.log(daysBetween)
+        const totalHours = Math.round(daysBetween * avghour);
+
+        const usedMem = Math.round(process.memoryUsage().rss / 1024 / 1024);
+        const usedHeap = Math.round(process.memoryUsage().heapUsed / 1024 / 1024);
+
+        const botLatency = Date.now() - interaction.createdTimestamp;
+        const apiLatency = Math.round(interaction.client.ws.ping);
+        const start = Date.now();
+        let dbLatency;
+        try {
+
+            await con.query('SELECT 1');
+            dbLatency = Date.now() - start;
+        } catch (error) {
+            console.error('Database error:', error);
+            dbLatency = 'Error';
+        }
+        const totalLines = await countLinesOfCode();
+        const embed = new EmbedBuilder()
+        .setTitle(`Statistics about Mutt`)
+        .setColor("DarkButNotBlack")
+        .setDescription(`${interaction.client.user} has been developed by <@1231383939101360260> for \`~${totalHours} hours\`, starting ${daysCoded}`)
+        .setThumbnail(interaction.client.user.displayAvatarURL())
+        .addFields(
+            {name: `Lines of code`, value: `\`${totalLines}\``, inline: true},
+            {name: `RAM usage`, value: `Currently: \`${usedMem} MB\`\nHeap: \`${usedHeap} MB\``, inline: true},
+            {name: `Latency`, value: `Bot: \`${moment.duration(botLatency).milliseconds()}ms\`\nDatabase: \`${dbLatency !== 'Error' ? moment.duration(dbLatency).milliseconds() + 'ms' : 'Error'}\`\nAPI: \`${moment.duration(apiLatency).milliseconds()}ms\``, inline: true}
+        )
+
+        interaction.reply({embeds: [embed]})
+        
+    }
+}
+
+async function countLinesOfCode() {
+    return new Promise(async (resolve, reject) => {
+        const jsfiles = await glob('**/*.js', { ignore: 'node_modules/**' });
+
+        let totalLines = 0;
+
+        jsfiles.forEach(file => {
+            const fileContents = fs.readFileSync(file, 'utf8');
+            totalLines += fileContents.split('\n').length;
+        })  
+
+        resolve(totalLines)
+    })
+    
+}
