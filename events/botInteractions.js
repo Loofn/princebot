cd7110165bcc0aa324b02abc4a42c9cd4839d376
Loofn <patrik.tuominen@outlook.com>
2024-09-30 00:21:41 +0300

    ✨ feat: Nea responses

--- events/botInteractions.js
+++ events/botInteractions.js
@@ -14,29 +14,45 @@ client.on('messageCreate', async msg => {
         msg.react('💕')
     }
     if(message.startsWith("hi mutt")){
-        if(userId === '1223325333168848898'){
-            msg.reply(`Hi babushka, did you bake me cookies? They better be chocolate chip or I am eating it out of your ass`)
-            msg.react('🍪')
-        }
-        else if(userId === '560112855026761789'){
-            msg.reply(`<a:Furr_cum:1233559991433297921> Oh so you are getting dominated by the same floof derg? How pathetic of us... `)
-            msg.react('🍆')
-        }
-        else if(userId === '526467575673257995'){
-            msg.reply(`38 shots? I can do 69... bitch!`)
-            msg.react('🍺')
-        }
-        else if(userId === '1008967621766557819'){
+        // Red
+        if(userId === '1008967621766557819'){
             msg.reply(`What dat ass do?! FUCKING HOT!`)
             msg.react('🍑')
         }
+        // Lofn
         else if(userId === '102756256556519424'){
             msg.reply(`Hi daddy!`)
             msg.react('💗')
         }
-        else if(userId === '1219436982695100466'){
-            msg.reply(`Hi there, I heard you like being naughty in cons... can I join you? You can use me behind the corner..!`)
-            msg.react('💦')
+        // Dice
+        else if(userId === '1252440817310896211'){
+            msg.reply(`Hi stinky, I might be a doggo but you are the real mutt *swinging rope ready to pin down the lion*`)
+            msg.react('<:Catto_UvU:1236762320248766484>')
+        }
+        // Danny
+        else if(userId === '1241864422834569298'){
+            msg.reply(`Hi dandan, can you switch bleach into my coom? It is way more healthier..`)
+            msg.react('<a:Lewd_CockCum:1233559991433297921>')
+        }
+        // Anon
+        else if(userId === '627949692894052369'){
+            msg.reply(`Hi ${msg.author}, I know what you did with Lofn... naughty naughty little princess!`)
+            msg.react('<a:Fox_Blush:1238204170645930014>')
+        }
+        // Winter
+        else if(userId === '213152760726552576'){
+            msg.reply(`*Shows sharp teeth at ${msg.author}* You know what these do...?`)
+            msg.react('🩸')
+        }
+        // Alice
+        else if(userId === '908024462312611840'){
+            msg.reply(`Proton goes beep boop, you freaking toaster!`)
+            msg.react('🍞')
+        }
+        //Vidraal
+        else if(userId === '1091590806026063963'){
+            msg.reply(`Heyo, I know your favorite channel is... <#1233561577920401429> you should post more...`)
+            msg.react('🔞')
         }
         else {
             var i = Math.floor(Math.random() * randomResponses.length);
