cfb76ef993fdd5f77d3b9dbe5575138a2b99cbd2
Loofn <patrik.tuominen@outlook.com>
2025-01-22 01:01:15 +0200

    ✨ feat: Added blackjack game

---
+++ events/gambling.js
@@ -0,0 +1,162 @@
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const client = require('..');
+
+const {noPerms} = require('../data/embeds');
+const serverRoles = require('../data/serverRoles.json')
+const serverChannels = require('../data/channels.json')
+const { isBooster } = require('../function/roles');
+const { givePoints } = require('../function/furrygame');
+
+client.on('interactionCreate', async interaction => {
+
+    if(interaction.isButton()){
+
+        // Blackjack interactions / playing
+        if(interaction.customId.toLowerCase().startsWith("bj")){
+
+            const options = interaction.customId.split('-');
+            const action = options[0];
+            const playerId = options[1];
+            const currentDice = options[2];
+
+            if(playerId !== interaction.member.id) {
+                return interaction.reply({content: `This is not your game... dummy...`, ephemeral: true})
+            }
+
+            let oldEmbed = interaction.message.embeds[0];
+            const betAmountMatch = oldEmbed.description.match(/Bet amount: \`(\d+)/)
+            const betAmount = parseInt(betAmountMatch[1], 10);
+
+            const handMatch = oldEmbed.description.match(/(Starting|Current) hand is: \`(\d+)/);
+            const currentHand = parseInt(handMatch[2], 10);
+
+            const hasBoosterRole = await isBooster(interaction.member.id);
+            let winnings = betAmount;
+            if(hasBoosterRole) {
+                winnings = winnings*1,75;
+            } else {
+                winnings = winnings*1,25;
+            }
+
+            if(action === 'bjr'){
+
+                const newDice = rollDice();
+                const newSum = sum(currentHand, newDice);
+
+                if(newSum > 21){
+                    const busted = new EmbedBuilder()
+                        .setTitle(`Oh no ${interaction.member.user.username} BUSTED their loadd..`)
+                        .setDescription(`**You threw:** \`${newDice}\` :game_die:\nBusted hand is: \`${newSum}\`\nLost bet amount is: \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243>`)
+                        .setThumbnail(`https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmlqM204MG1ieTl2MDU1czJjejY4aWlpdmZ2OXc4eGY4OG9zN2JrMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1aFfcTbMqVI52/giphy.gif`)
+                        .setColor("Red")
+
+                    interaction.message.edit({embeds: [busted], components: []}).then(() => {
+                        interaction.reply({content: `You **BUSTED**!! *moans* well.. better luck next time!`, ephemeral: true})
+                    })
+                } else if(newSum == 21) {
+                    const blackJack = new EmbedBuilder()
+                        .setTitle(`${interaction.member.user.username} scored a fking BLACKJACK!`)
+                        .setColor("Gold")
+                        .setThumbnail(`https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjNuN2Ixd2JscTZja3AyZXBxNTB2c2FsaGJ2a3gxa2tkNWo2YTB4diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PIdyzBZ8XiKQWfgwYk/giphy.gif`)
+                        .setDescription(`**You threw:** \`${newDice}\` :game_die:\nCurrent hand is: \`${newSum}\`\nBet amount was: \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243>\n\n**You won:** \`${winnings} cumcoins\` <a:Lewd_Coom:1235063571868680243>`)
+                    
+                    interaction.message.edit({embeds: [blackJack], components: []}).then(() => {
+                        interaction.reply({content: `You **WON**!! *moans*!`, ephemeral: true})
+                    })
+                    givePoints(interaction.member.id, winnings);
+
+                } else {
+                    const newEmbed = new EmbedBuilder()
+                    .setTitle(oldEmbed.title)
+                    .setFooter({text: `Winnings are x1.25 for regulars, and x1.75 for server boosters`, iconURL: interaction.guild.iconURL()})
+                    .setDescription(`**You threw:** \`${newDice}\` :game_die:\nCurrent hand is: \`${newSum}\`\nBet amount: \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243>`)
+
+                    interaction.message.edit({embeds: [newEmbed]}).then(() => {
+                        interaction.reply({content: `You **threw ${newDice}**!`, ephemeral: true})
+                    })
+                }
+
+                
+            }
+
+            if(action === 'bjs'){
+                await interaction.deferReply({ephemeral: true});
+                let dealerTotal = sum(rollDice(), rollDice());
+
+                let stayEmbed = new EmbedBuilder()
+                    .setTitle(`${interaction.member.user.username} decided to stay!`)
+                    .setDescription(`**You stayed!** 🖐️\n\nCurrent hand is: \`${currentHand}\`\nBet amount is: \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243>`)
+                    .addFields(
+                        {name: `Dealer throwing...`, value: `:game_die: Rolled: \`${dealerTotal}\``}
+                    )
+                await interaction.message.edit({embeds: [stayEmbed], components: []})
+                await new Promise((resolve) => setTimeout(resolve, 2000));
+
+                while (dealerTotal < 17){
+                    const newRoll = rollDice()
+                    dealerTotal += newRoll;
+                    stayEmbed.addFields({name:`** **`, value: `\n:game_die: Rolled: \`+${newRoll}\` (\`${dealerTotal}\`)`}) 
+
+                    await new Promise((resolve) => setTimeout(resolve, 2000));
+                    await interaction.message.edit({embeds: [stayEmbed]});
+                }
+
+                if(dealerTotal > 21) {
+                    // WIN
+                    const wonGame = new EmbedBuilder()
+                        .setTitle(`${interaction.member.user.username} won! Dealer busted ~~a nut~~`)
+                        .setColor("Gold")
+                        .setThumbnail(`https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjNuN2Ixd2JscTZja3AyZXBxNTB2c2FsaGJ2a3gxa2tkNWo2YTB4diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PIdyzBZ8XiKQWfgwYk/giphy.gif`)
+                        .setDescription(`**You stayed!** 🖐️\n\nCurrent hand is: \`${currentHand}\`\nDealer's hand is: \`${dealerTotal}\`\nBet amount was: \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243>\n\n**You won:** \`${winnings} cumcoins\` <a:Lewd_Coom:1235063571868680243>`)
+                    
+                    interaction.message.edit({embeds: [wonGame], components: []}).then(() => {
+                        interaction.editReply({content: `You **WON**!! *moans*`, ephemeral: true})
+                    })
+                    givePoints(interaction.member.id, winnings);
+                } else if (currentHand > dealerTotal) {
+                    // WIN
+                    const wonGame = new EmbedBuilder()
+                        .setTitle(`${interaction.member.user.username} won! Dealer busted ~~a nut~~ faster`)
+                        .setColor("Gold")
+                        .setThumbnail(`https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjNuN2Ixd2JscTZja3AyZXBxNTB2c2FsaGJ2a3gxa2tkNWo2YTB4diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PIdyzBZ8XiKQWfgwYk/giphy.gif`)
+                        .setDescription(`**You stayed!** 🖐️\n\nCurrent hand is: \`${currentHand}\`\nDealer's hand is: \`${dealerTotal}\`\nBet amount was: \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243>\n\n**You won:** \`${winnings} cumcoins\` <a:Lewd_Coom:1235063571868680243>`)
+                    
+                    interaction.message.edit({embeds: [wonGame], components: []}).then(() => {
+                        interaction.editReply({content: `You **WON**!! *moans*`, ephemeral: true})
+                    })
+                    givePoints(interaction.member.id, winnings);
+                } else if (currentHand == dealerTotal){
+                    // TIE
+                    const wonGame = new EmbedBuilder()
+                        .setTitle(`${interaction.member.user.username} and dealer were both on the edge...`)
+                        .setColor("Gold")
+                        .setThumbnail(`https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjNuN2Ixd2JscTZja3AyZXBxNTB2c2FsaGJ2a3gxa2tkNWo2YTB4diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PIdyzBZ8XiKQWfgwYk/giphy.gif`)
+                        .setDescription(`**You stayed!** 🖐️\n\nCurrent hand is: \`${currentHand}\`\nDealer's hand is: \`${dealerTotal}\`\nBet amount was: \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243>\n\n**You won:** \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243>`)
+                    
+                    interaction.message.edit({embeds: [wonGame], components: []}).then(() => {
+                        interaction.editReply({content: `You tied... which means you got nothing...`, ephemeral: true})
+                    })
+                    givePoints(interaction.member.id, betAmount);
+                } else {
+                    // LOSE
+                    const wonGame = new EmbedBuilder()
+                        .setTitle(`${interaction.member.user.username} busted all over the Dealer; Dealer wins!`)
+                        .setColor("Gold")
+                        .setThumbnail(`https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjNuN2Ixd2JscTZja3AyZXBxNTB2c2FsaGJ2a3gxa2tkNWo2YTB4diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PIdyzBZ8XiKQWfgwYk/giphy.gif`)
+                        .setDescription(`**You stayed!** 🖐️\n\nCurrent hand is: \`${currentHand}\`\nDealer's hand is: \`${dealerTotal}\`\nBet amount was: \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243> *and you lost all of it...*`)
+                    
+                    interaction.message.edit({embeds: [wonGame], components: []}).then(() => {
+                        interaction.editReply({content: `You **LOST**!! *cries*`, ephemeral: true})
+                    })
+                }
+            }
+            
+        } else return;
+    }
+})
+
+const rollDice = () => Math.floor(Math.random() * 6) + 1;
+
+const sum = (...numbers) => {
+    return numbers.reduce((total, num) => total + num, 0);
+}
---
+++ slashCommands/gambling/blackjack.js
@@ -0,0 +1,86 @@
+const { ApplicationCommandType, EmbedBuilder, ChannelType, ButtonBuilder, ButtonStyle, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const serverRoles = require('../../data/serverRoles.json')
+const con = require('../../function/db');
+const moment = require('moment');
+const { getPoints, removePoints } = require('../../function/furrygame');
+
+
+module.exports = {
+    name: 'blackjack',
+    description: 'Play some blackjack with optional bet',
+    cooldown: 3000,
+    type: ApplicationCommandType.ChatInput,
+    options: [
+        
+        {
+            name: 'bet',
+            description: 'Choose how many cumcoins you want to bet',
+            type: 4,
+            required: false,
+            min_value: 1
+        }
+
+    ],
+
+    run: async (client, interaction) => {
+        const { member, channelId, guildId, applicationId, 
+            commandName, deferred, replied, ephemeral, 
+            options, id, createdTimestamp 
+        } = interaction; 
+        const { guild } = member;
+
+
+        const betAmount = options.getInteger('bet') ? options.getInteger('bet') : 0;
+
+        const playerDie1 = rollDice();
+        const playerDie2 = rollDice();
+
+        const rollBtn = new ButtonBuilder()
+            .setCustomId(`bjr-${member.id}-${sum(playerDie1, playerDie2)}`)
+            .setLabel(`Roll more`)
+            .setStyle(ButtonStyle.Primary)
+            .setEmoji('🎲')
+        
+        const stayBtn = new ButtonBuilder()
+            .setCustomId(`bjs-${member.id}-${sum(playerDie1, playerDie2)}`)
+            .setLabel(`Stay`)
+            .setEmoji('🖐️')
+            .setStyle(ButtonStyle.Danger)
+
+        const row = new ActionRowBuilder().addComponents(rollBtn, stayBtn)
+
+        const gameEmbed = new EmbedBuilder()
+            .setTitle(`Blackjack game of ${member.user.username}`)
+            .setDescription(`Starting hand is: \`${sum(playerDie1, playerDie2)}\`\nBet amount: \`${betAmount} cumcoins\` <a:Lewd_Coom:1235063571868680243>\n\n**Good luck** :pray:`)
+            .setFooter({text: `Winnings are x1.25 for regulars, and x1.75 for server boosters`, iconURL: interaction.guild.iconURL()})
+
+        if(betAmount == 0){
+            interaction.reply({embeds: [gameEmbed], components: [row]});
+        } else {
+            const pointBalance = await getPoints(member.id);
+
+            if(pointBalance >= betAmount){
+                removePoints(member.id, betAmount);
+                interaction.reply({embeds: [gameEmbed], components: [row]});
+            } else {
+                const notEnoughPoints = new EmbedBuilder()
+                    .setTitle(`Not enough cum to play...`)
+                    .setColor("DarkRed")
+                    .setDescription(`You attempted to gamble \`${betAmount} cumcoins\` but you have \`${pointBalance} cumcoins\`.. The math does not math dear`)
+                    .setThumbnail(`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ256eGxzaHg1eDQwNnZodncyMzB6MDdqaW5qNzZoNmFycWlxNjNsdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/cRHgphdnVZMtRLZlT1/giphy.gif`)
+            
+                await interaction.reply({embeds: [notEnoughPoints]}).then(msg => {
+                    setTimeout(() => {
+                        msg.delete()
+                    }, 10000);
+                })
+            }
+        }
+    }
+}
+
+const rollDice = () => Math.floor(Math.random() * 6) + 1;
+
+const sum = (...numbers) => {
+    return numbers.reduce((total, num) => total + num, 0);
+}
